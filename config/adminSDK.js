9e673fac2fb2df2a8b0b3725342d7d9d68d60bb4
HexVanilla <97599030+HexVanilla@users.noreply.github.com>
2023-09-27 12:55:37 -0300

    config env change

--- config/adminSDK.js
+++ config/adminSDK.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
+const privateKeyBase64 = process.env.FIREBASE_PRIVATE_KEY
+const privateKey = Buffer.from(privateKeyBase64, 'base64').toString('utf-8')
 
 const adminConfig = {
   type: process.env.FIREBASE_TYPE,
   project_id: process.env.FIREBASE_PROJECT_ID,
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  private_key: privateKey,
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_CLIENT_ID,
   auth_uri: process.env.FIREBASE_AUTH_URI,
